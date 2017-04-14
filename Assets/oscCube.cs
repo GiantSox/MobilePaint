c5231de7d84a6da8bdbd6c7dde4979595fab642e
David Goodman <thegiantsox@gmail.com>
2017-04-13 22:16:33 -0700

    initial commit

---
+++ Assets/oscCube.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityOSC;
+
+public class oscCube : MonoBehaviour
+{
+
+    private Thread thread;
+    private OSCServer oscin;
+    Vector3 head;
+    Vector3 leftHand;
+    Vector3 rightHand;
+
+    void Start()
+    {
+        oscin = new OSCServer(12345);
+        Debug.Log("osc server started");
+        //thread = new Thread(new ThreadStart(UpdateOSC));
+        //thread.Start();
+    }
+
+    void OnApplicationQuit()
+    {
+        oscin.Close();
+        /*thread.Interrupt();
+        if (!thread.Join(2000))
+        {
+            thread.Abort();
+        }*/
+    }
+
+    /*void UpdateOSC()
+    {
+        while (true)
+        {
+            OSCPacket msg = oscin.LastReceivedPacket;
+            if (msg != null)
+            {
+                if (msg.IsBundle())
+                {
+                    OSCBundle b = (OSCBundle)msg;
+                    foreach (OSCPacket subm in b.Values)
+                    {
+                        parseMessage(subm);
+                    }
+                }
+                else {
+                    parseMessage(msg);
+                }
+            }
+            //Thread.Sleep(5);
+        }
+    }*/
+
+    void parseMessage(OSCPacket msg)
+    {
+        Debug.Log("message with address: " + msg.Address + " and value: " + msg.Data[0]);
+        if (msg.Address == "/head/")
+        {
+            head.x = (float)msg.Data[0];
+            head.y = (float)msg.Data[1];
+            //head.z = (float)msg.Data[2];
+        }/*
+        else if (msg.Address == "/leftHand/")
+        {
+            leftHand.x = (float)msg.Data[0];
+            leftHand.y = (float)msg.Data[1];
+            //leftHand.z = (float)msg.Data[2];
+        }
+        else if (msg.Address == "/rightHand/")
+        {
+            rightHand.x = (float)msg.Data[0];
+            rightHand.y = (float)msg.Data[1];
+            //rightHand.z = (float)msg.Data[2];
+        }*/
+    }
+
+    void Update()
+    {
+        OSCPacket message = oscin.LastReceivedPacket;
+        if(message != null)
+        {
+            parseMessage(message);
+        }
+        transform.position = head;
+
+    }
+}
