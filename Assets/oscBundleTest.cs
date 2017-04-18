5db775880fbb778f9528d5253f1656a359b65f30
David Goodman <thegiantsox@gmail.com>
2017-04-17 22:13:31 -0700

    osc bundle testing

---
+++ Assets/oscBundleTest.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityOSC;
+
+public class oscBundleTest : MonoBehaviour {
+
+    
+    private Thread thread;
+    private OSCServer oscin;
+
+
+    void Start()
+    {
+        oscin = new OSCServer( 31337 );
+        thread = new Thread(new ThreadStart(UpdateOSC));
+        thread.Start();
+    }
+
+    void OnApplicationQuit()
+    {
+        oscin.Close();
+        thread.Interrupt();
+        if (!thread.Join(2000))
+        {
+            thread.Abort();
+        }
+    }
+
+    void UpdateOSC()
+    {
+        while (true)
+        {
+            OSCPacket msg = oscin.LastReceivedPacket;
+            if (msg != null)
+            {
+                if (msg.IsBundle())
+                {
+                    OSCBundle b = (OSCBundle)msg;
+                    foreach (OSCPacket subm in b.Data)
+                    {
+                        parseMessage(subm);
+                    }
+                }
+                else {
+                    parseMessage(msg);
+                }
+            }
+            Thread.Sleep(5);
+        }
+    }
+
+    void parseMessage(OSCPacket msg)
+    {
+        Debug.Log("message with address: " + msg.Address);
+        // get a value:
+        int v = (int)msg.Data[0];
+    }
+
+    void Update()
+    {
+    }
+}
